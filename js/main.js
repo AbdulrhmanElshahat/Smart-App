a826ca5bb0498c9c00e4a332212728b8c00f391f
AbdulrhmanElshahat <https://AbdulrhmanElshahat@github.com>
2022-05-25 04:21:39 +0200

    play buttons

--- js/main.js
+++ js/main.js
@@ -1,28 +1,17 @@
-const questions = document.querySelectorAll(".questions-section li"); 
-const answerContainer = document.querySelector(".answer");
-const answers = [
-  "عادي مينفعش ليه",
-  " مينفعش ليه",
-  "عادي مينفعش ",
-  "عادي ليه",
-];
+const links = document.querySelectorAll(".container a");
 
-answerContainer.textContent = answers[0]; //set first answer default
+function keys(ele){
+    document.addEventListener("keydown", function(event){
+        let theKey = event.key
+        for(let i = 0 ; i < ele.length ; i++){
+            if(theKey == ele[i].children[0].textContent){
+                ele[i].click()
+            }
+        }
+    
+    })
+}
 
-questions.forEach((question, index) => {
-  question.onclick = (_) => {
-      //remove all active calss from questions
-    for (let i = 0; i < questions.length; i++) {
-      questions[i].querySelector(".row").classList.remove("active");
-    }
-    //add active calss on clicked question
-    question.querySelector(".row").classList.add("active");
-    //set answer of selected question
-    answerContainer.textContent = answers[index];
-    //scroll to answer when click
-    if (window.innerWidth < 912) {
-      window.scrollTo(0, answerContainer.offsetTop);
-     
-    }
-  };
-});
+keys(links)
+if(window.location.pathname === '/questions.html'){keys(questions) ; console.log("Eshta ")}
+
---
+++ js/questions.js
@@ -0,0 +1,29 @@
+const questions = document.querySelectorAll(".questions-section li"); 
+const answerContainer = document.querySelector(".answer");
+const answers = [
+  "عادي مينفعش ليه",
+  " مينفعش ليه",
+  "عادي مينفعش ",
+  "عادي ليه",
+];
+
+answerContainer.textContent = answers[0]; //set first answer default
+
+questions.forEach((question, index) => {
+  question.onclick = (_) => {
+      //remove all active calss from questions
+    for (let i = 0; i < questions.length; i++) {
+      questions[i].querySelector(".row").classList.remove("active");
+    }
+    //add active calss on clicked question
+    question.querySelector(".row").classList.add("active");
+    //set answer of selected question
+    answerContainer.textContent = answers[index];
+    //scroll to answer when click
+    if (window.innerWidth < 912) {
+      window.scrollTo(0, answerContainer.offsetTop);
+     
+    }
+  };
+});
+
